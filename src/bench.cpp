5200301ae532fe26aa159efbda83d39902a4a112
Jan Sordid <jan@sordid.de>
2024-06-05 16:02:15 +0200

    add constexpr fib to tests

--- src/bench.cpp
+++ src/bench.cpp
@@ -5,6 +5,37 @@
 
 using namespace JanSordid::Core;
 
+//consteval
+constexpr long long fib( long long n )
+{
+    AssertInOptimized( n >= 0 );
+    /*
+    return (n == 0) ? 0
+         : (n == 1) ? 1
+         : fib( n - 1 )
+         + fib( n - 2 );
+    //*/
+    //*
+    if( n == 0 ) return 0;
+    if( n == 1 ) return 1;
+    return fib( n - 1 )
+         + fib( n - 2 );
+    //*/
+}
+
+static void BM_Fibonacci( benchmark::State & state )
+{
+    for( auto _ : state )
+    {
+        // This code gets timed
+        const int64_t paramForFib = state.range( 0 );
+        constexpr int64_t result = fib( 27 ); // muss zur CT ausgerechnet werden
+        //benchmark::DoNotOptimize( result );
+        int64_t result2 = fib( paramForFib );
+        //benchmark::DoNotOptimize( result2 );
+    }
+}
+/*
 static void BM_MatrixMulF( benchmark::State & state )
 {
     const usize size = state.range( 0 ); // Not used yet
@@ -28,6 +59,7 @@ static void BM_MatrixMulD( benchmark::State & state )
         a *= b;
     }
 }
+//*/
 
 static void BM_CreateString( benchmark::State & state )
 {
@@ -41,14 +73,21 @@ static void BM_CreateString( benchmark::State & state )
     }
 }
 
+BENCHMARK( BM_Fibonacci )
+->DenseRange( 0, 47, 1 );
+
+/*
 BENCHMARK( BM_MatrixMulF )
     ->Range( 1<<4, 1<<12 );
 
 BENCHMARK( BM_MatrixMulD )
     ->Range( 1<<4, 1<<12 );
+//*/
 
 BENCHMARK( BM_CreateString )
 	->RangeMultiplier( 2 )
 	->Range( 1<<0, 1<<13 );
 
 BENCHMARK_MAIN();
+
+// TODO: read up on doctest
